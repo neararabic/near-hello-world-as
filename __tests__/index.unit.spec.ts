3b7618bd5120fe9d848371637b7df9c289aa1e97
mohammed <msaudi.cse@gmail.com>
2022-02-14 20:39:23 +0300

    fixed test file

--- __tests__/index.unit.spec.ts
+++ __tests__/index.unit.spec.ts
@@ -1,8 +1,6 @@
 import { sayHello } from "../assembly/index";
 
 describe("Contract", () => {
-  // VIEW method tests
-
   it("says hello", () => {
     expect(sayHello()).toStrictEqual("hello from the blockchain world!");
   });
